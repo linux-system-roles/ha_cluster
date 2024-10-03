5e60a419a43c7cd1a5ee4b36fa5757156246e6dd
Tomas Jelinek <tojeline@redhat.com>
2024-10-03 15:18:31 +0200

    refactor: ha_cluster_info: split data loading and transformation
    Implementing changes proposed in code review

--- library/ha_cluster_info.py
+++ library/ha_cluster_info.py
@@ -74,12 +74,23 @@ ha_cluster:
 
 import json
 import os.path
-from typing import Any, Dict, List, Optional
+from typing import Any, Callable, Dict, List, Optional, Tuple
 
 from ansible.module_utils.basic import AnsibleModule
 
 COROSYNC_CONF_PATH = "/etc/corosync/corosync.conf"
 KNOWN_HOSTS_PATH = "/var/lib/pcsd/known-hosts"
+
+
+CommandRunner = Callable[
+    # parameters: args, environ_update
+    # environ_update should be a keyword argument, but they are not possible in
+    # Callable. typing.Protocol would have to be used to type that, but that is
+    # not available in Python 3.6
+    [List[str], Optional[Dict[str, str]]],
+    # return value: rc, stdout, stderr
+    Tuple[int, str, str],
+]
 
 
 class CliCommandError(Exception):
@@ -156,14 +167,10 @@ class JsonMissingKey(Exception):
         return dict(key=self.key, data=self.data, data_desc=self.data_desc)
 
 
-def dict_to_nv_list(input_dict: Dict[str, Any]) -> List[Dict[str, Any]]:
-    """
-    Convert a dict to a list of dicts with keys 'name' and 'value'
-    """
-    return [dict(name=name, value=value) for name, value in input_dict.items()]
+# functions loading data from cluster
 
 
-def is_service_enabled(module: AnsibleModule, service: str) -> bool:
+def is_service_enabled(run_command: CommandRunner, service: str) -> bool:
     """
     Check whether a specified service is enabled in the OS
 
@@ -174,15 +181,24 @@ def is_service_enabled(module: AnsibleModule, service: str) -> bool:
         "LC_ALL": "C",
     }
     # wokeignore:rule=dummy
-    rc, dummy_stdout, dummy_stderr = module.run_command(
-        ["systemctl", "is-enabled", f"{service}.service"],
-        check_rc=False,
-        environ_update=env,
+    rc, dummy_stdout, dummy_stderr = run_command(
+        ["systemctl", "is-enabled", f"{service}.service"], env
     )
     return rc == 0
 
 
-def call_pcs_cli(module: AnsibleModule, command: List[str]) -> Dict[str, Any]:
+def load_start_on_boot(run_command: CommandRunner) -> bool:
+    """
+    Detect wheter a cluster is configured to start on boot
+    """
+    return is_service_enabled(run_command, "corosync") or is_service_enabled(
+        run_command, "pacemaker"
+    )
+
+
+def call_pcs_cli(
+    run_command: CommandRunner, command: List[str]
+) -> Dict[str, Any]:
     """
     Run pcs CLI with the specified command, transform resulting JSON into a dict
 
@@ -193,11 +209,7 @@ def call_pcs_cli(module: AnsibleModule, command: List[str]) -> Dict[str, Any]:
         "LC_ALL": "C",
     }
     full_command = ["pcs"] + command
-    rc, stdout, stderr = module.run_command(
-        full_command,
-        check_rc=False,
-        environ_update=env,
-    )
+    rc, stdout, stderr = run_command(full_command, env)
     if rc != 0:
         raise CliCommandError(full_command, rc, stdout, stderr)
     try:
@@ -208,84 +220,13 @@ def call_pcs_cli(module: AnsibleModule, command: List[str]) -> Dict[str, Any]:
         ) from e
 
 
-def export_start_on_boot(module: AnsibleModule) -> bool:
+def load_corosync_conf(run_command: CommandRunner) -> Dict[str, Any]:
     """
-    Detect wheter a cluster is configured to start on boot
+    Get corosync configuration from pcs
     """
-    return is_service_enabled(module, "corosync") or is_service_enabled(
-        module, "pacemaker"
+    return call_pcs_cli(
+        run_command, ["cluster", "config", "--output-format=json"]
     )
-
-
-def export_corosync_conf(module: AnsibleModule) -> Dict[str, Any]:
-    """
-    Export corosync configuration
-    """
-    conf_dict = call_pcs_cli(
-        module, ["cluster", "config", "--output-format=json"]
-    )
-    result: Dict[str, Any] = dict()
-    try:
-        result["ha_cluster_cluster_name"] = conf_dict["cluster_name"]
-
-        transport = dict(type=conf_dict["transport"].lower())
-        if conf_dict["transport_options"]:
-            transport["options"] = dict_to_nv_list(
-                conf_dict["transport_options"]
-            )
-        if conf_dict["links_options"]:
-            link_list = []
-            for link_dict in conf_dict["links_options"].values():
-                # linknumber is an index in links_options, but it is present in
-                # link_dict as well
-                link_list.append(dict_to_nv_list(link_dict))
-            transport["links"] = link_list
-        if conf_dict["compression_options"]:
-            transport["compression"] = dict_to_nv_list(
-                conf_dict["compression_options"]
-            )
-        if conf_dict["crypto_options"]:
-            transport["crypto"] = dict_to_nv_list(conf_dict["crypto_options"])
-        result["ha_cluster_transport"] = transport
-
-        if conf_dict["totem_options"]:
-            result["ha_cluster_totem"] = dict(
-                options=dict_to_nv_list(conf_dict["totem_options"])
-            )
-        if conf_dict["quorum_options"]:
-            result["ha_cluster_quorum"] = dict(
-                options=dict_to_nv_list(conf_dict["quorum_options"])
-            )
-
-        if conf_dict["nodes"]:
-            node_list = []
-            for index, node_dict in enumerate(conf_dict["nodes"]):
-                try:
-                    node_list.append(
-                        dict(
-                            node_name=node_dict["name"],
-                            corosync_addresses=[
-                                addr_dict["addr"]
-                                for addr_dict in sorted(
-                                    node_dict["addrs"],
-                                    key=lambda item: item["link"],
-                                )
-                            ],
-                        )
-                    )
-                except KeyError as e:
-                    raise JsonMissingKey(
-                        e.args[0],
-                        conf_dict,
-                        f"corosync configuration for node on index {index}",
-                    ) from e
-            result["ha_cluster_node_options"] = node_list
-
-    except KeyError as e:
-        raise JsonMissingKey(
-            e.args[0], conf_dict, "corosync configuration"
-        ) from e
-    return result
 
 
 def load_pcsd_known_hosts() -> Dict[str, str]:
@@ -319,25 +260,121 @@ def load_pcsd_known_hosts() -> Dict[str, str]:
         raise JsonParseError(str(e), "not logging data", "known hosts") from e
 
 
-def merge_known_hosts(
-    result: Dict[str, Any], node_addr: Dict[str, str]
-) -> None:
-    """
-    Merge pcs node addresses into ha_cluster_node_options key
+# functions transforming data from pcs format to role format
 
-    result -- structure with exported corosync configuration
-    node_addr -- node_name: node_addr map loaded from known hosts
+
+def dict_to_nv_list(input_dict: Dict[str, Any]) -> List[Dict[str, Any]]:
     """
-    # node_addr may contain records for nodes which are not part of the cluster
-    # being exported. These are ignored. We are only interested in pcs
-    # addresses of nodes forming the exported cluster.
-    if not node_addr:
-        return
-    if "ha_cluster_node_options" not in result:
-        return
-    for node_options in result["ha_cluster_node_options"]:
-        if node_options["node_name"] in node_addr:
-            node_options["pcs_address"] = node_addr[node_options["node_name"]]
+    Convert a dict to a list of dicts with keys 'name' and 'value'
+    """
+    return [dict(name=name, value=value) for name, value in input_dict.items()]
+
+
+def export_corosync_options(
+    corosync_conf_dict: Dict[str, Any]
+) -> Dict[str, Any]:
+    """
+    Transform corosync config from pcs format to role format excluding nodes
+
+    corosync_conf_dict -- corosync config structure provided by pcs
+    """
+    result: Dict[str, Any] = dict()
+    try:
+        result["ha_cluster_cluster_name"] = corosync_conf_dict["cluster_name"]
+
+        transport = dict(type=corosync_conf_dict["transport"].lower())
+        if corosync_conf_dict["transport_options"]:
+            transport["options"] = dict_to_nv_list(
+                corosync_conf_dict["transport_options"]
+            )
+        if corosync_conf_dict["links_options"]:
+            link_list = []
+            for link_dict in corosync_conf_dict["links_options"].values():
+                # linknumber is an index in links_options, but it is present in
+                # link_dict as well
+                link_list.append(dict_to_nv_list(link_dict))
+            transport["links"] = link_list
+        if corosync_conf_dict["compression_options"]:
+            transport["compression"] = dict_to_nv_list(
+                corosync_conf_dict["compression_options"]
+            )
+        if corosync_conf_dict["crypto_options"]:
+            transport["crypto"] = dict_to_nv_list(
+                corosync_conf_dict["crypto_options"]
+            )
+        result["ha_cluster_transport"] = transport
+
+        if corosync_conf_dict["totem_options"]:
+            result["ha_cluster_totem"] = dict(
+                options=dict_to_nv_list(corosync_conf_dict["totem_options"])
+            )
+
+        if corosync_conf_dict["quorum_options"]:
+            result["ha_cluster_quorum"] = dict(
+                options=dict_to_nv_list(corosync_conf_dict["quorum_options"])
+            )
+    except KeyError as e:
+        raise JsonMissingKey(
+            e.args[0], corosync_conf_dict, "corosync configuration"
+        ) from e
+    return result
+
+
+def export_cluster_nodes(
+    corosync_conf_nodes: List[Dict[str, Any]], pcs_node_addr: Dict[str, str]
+) -> List[Dict[str, Any]]:
+    """
+    Transform node configuration from pcs format to role format
+
+    corosync_conf_dict -- corosync config structure provided by pcs
+    pcs_node_addr -- dict holding pcs address for cluster nodes
+    """
+    node_list: List[Dict[str, Any]] = []
+    if not corosync_conf_nodes:
+        return node_list
+    for index, node_dict in enumerate(corosync_conf_nodes):
+        # corosync node configuration
+        try:
+            one_node = dict(
+                node_name=node_dict["name"],
+                corosync_addresses=[
+                    addr_dict["addr"]
+                    for addr_dict in sorted(
+                        node_dict["addrs"],
+                        key=lambda item: item["link"],
+                    )
+                ],
+            )
+        except KeyError as e:
+            raise JsonMissingKey(
+                e.args[0],
+                dict(nodes=corosync_conf_nodes),
+                f"corosync configuration for node on index {index}",
+            ) from e
+        # pcs node configuration
+        if one_node["node_name"] in pcs_node_addr:
+            one_node["pcs_address"] = pcs_node_addr[one_node["node_name"]]
+        # finish one node export
+        node_list.append(one_node)
+    return node_list
+
+
+# ansible module tools and top layer functions
+
+
+def get_cmd_runner(module: AnsibleModule) -> CommandRunner:
+    """
+    Provide a function responsible for running external processes
+    """
+
+    def runner(
+        args: List[str], environ_update: Optional[Dict[str, str]] = None
+    ) -> Tuple[int, str, str]:
+        return module.run_command(
+            args, check_rc=False, environ_update=environ_update
+        )
+
+    return runner
 
 
 def export_cluster_configuration(module: AnsibleModule) -> Dict[str, Any]:
@@ -348,20 +385,41 @@ def export_cluster_configuration(module: AnsibleModule) -> Dict[str, Any]:
     # put it together from separate parts provided by pcs. Some parts are only
     # available in recent pcs versions. Check pcs capabilities.
     result: dict[str, Any] = dict()
+    cmd_runner = get_cmd_runner(module)
 
-    result["ha_cluster_start_on_boot"] = export_start_on_boot(module)
+    result["ha_cluster_start_on_boot"] = load_start_on_boot(cmd_runner)
 
     # Corosync config is availabe via CLI since pcs-0.10.8, via API v2 since
     # pcs-0.12.0 and pcs-0.11.9. For old pcs versions, CLI must be used, and
     # there is no benefit in implementing access via API on top of that.
     # No need to check pcs capabilities. If this is not supported by pcs,
     # exporting anything else is pointless (and not supported by pcs anyway).
-    result.update(**export_corosync_conf(module))
-
+    corosync_conf_pcs = load_corosync_conf(cmd_runner)
     # known-hosts file is available since pcs-0.10, but is not exported by pcs
     # in any version.
     # No need to check pcs capabilities.
-    merge_known_hosts(result, load_pcsd_known_hosts())
+    known_hosts_pcs = load_pcsd_known_hosts()
+
+    # Convert corosync config to role format
+    corosync_conf_role = export_corosync_options(corosync_conf_pcs)
+    for key in (
+        "ha_cluster_cluster_name",
+        "ha_cluster_transport",
+        "ha_cluster_totem",
+        "ha_cluster_quorum",
+    ):
+        if key in corosync_conf_role:
+            result[key] = corosync_conf_role[key]
+
+    # Convert cluster definition to role format
+    try:
+        result["ha_cluster_node_options"] = export_cluster_nodes(
+            corosync_conf_pcs["nodes"], known_hosts_pcs
+        )
+    except KeyError as e:
+        raise JsonMissingKey(
+            e.args[0], corosync_conf_pcs, "corosync configuration"
+        ) from e
 
     return result
 
