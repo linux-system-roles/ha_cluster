938ed779181df4be0257d10ef00f4d3c35b448d5
Ivan Devat <idevat@redhat.com>
2025-10-29 09:10:23 +0100

    chore: deal with known ansible sanity issues

--- module_utils/ha_cluster_lsr/info/exporter_package/constraint_options.py
+++ module_utils/ha_cluster_lsr/info/exporter_package/constraint_options.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2024 Red Hat, Inc.
+# Author: Tomas Jelinek <tojeline@redhat.com>
+# SPDX-License-Identifier: MIT
+
+# make ansible-test happy, even though the module requires Python 3
+from __future__ import absolute_import, division, print_function
+
+# make ansible-test happy, even though the module requires Python 3
+# pylint: disable=invalid-name
+__metaclass__ = type
+
 from typing import Any, Dict, List, Tuple, Union
 
 from .wrap_src import SrcDict, cleanup_wrap
